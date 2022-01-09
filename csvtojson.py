962bf718c0c33e8c00c11ef8df2037950112d7db
rachidkarakhi <rachidkarakhi100@gmail.com>
2022-01-09 19:46:59 +0100

    first commit

---
+++ csvtojson.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv (r'D:\computer science and technology\s1\ia\projects\finale\topical_chat.csv')
+df.to_json (r'D:\computer science and technology\s1\ia\projects\finale\topical_chat.json')
